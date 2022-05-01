1f6f9d9f76ce9e86862d1a2ad99cbeb71e3b2e33
KseniyaShestakova <91065721+KseniyaShestakova@users.noreply.github.com>
2022-05-01 13:18:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+from internal_logic import *
 from file_parsing import *
+import sys
 
 # ожидается ввод типа <name of the file> <path_to_parameters> <path_to_nodes>
-# вязанные файлы, указанные в импортах сверху, должны лежать в той же папке
-
+# связанные файлы, указанные в импортах сверху, должны лежать в той же папке
 parser = Parser()
-state = parser.parse('parameters.txt', 'positions.txt')
+state = parser.parse(sys.argv[1], sys.argv[2])
 state.start()
